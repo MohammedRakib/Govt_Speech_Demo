51fee7423b0865f6fe46fed3ed6c1b0dbb7d32c5
MohammedRakib <rafz.rakib@gmail.com>
2023-02-05 23:08:23 +0600

    Minor Changes

---
+++ wav2vec2-normal/gradio-app.py
@@ -0,0 +1,131 @@
+import gradio as gr
+import pytube as pt
+import subprocess
+from models import asr
+# from punctuate.punctuate import punctuator
+import time
+import librosa
+## details: https://huggingface.co/docs/diffusers/optimization/fp16#automatic-mixed-precision-amp
+# from torch import autocast 
+
+def _preprocess(filename):
+     audio_name = "audio.wav"
+     subprocess.call(['ffmpeg', '-y', '-i', filename, '-acodec', 'pcm_s16le', '-ar', '16000', '-ac', '1', "-loglevel", "quiet", audio_name])
+     return audio_name
+ 
+def transcribe(microphone, file_upload):
+    warn_output = ""
+    if (microphone is not None) and (file_upload is not None):
+        warn_output = (
+            "WARNING: You've uploaded an audio file and used the microphone. "
+            "The recorded file from the microphone will be used and the uploaded audio will be discarded.\n"
+        )
+
+    elif (microphone is None) and (file_upload is None):
+        return "ERROR: You have to either use the microphone or upload an audio file"
+
+    file = microphone if microphone is not None else file_upload
+    print(f"\n\nFile is: {file}\n\n")
+    
+    # for _preprocess(). No need if name of file provided in string format to asr pipeline as automatically uses ffmeg. 
+    # Only required if ndarray given by using librosa.load() to load a file
+    start_time = time.time()
+    print("Starting Preprocessing")
+    speech_array = _preprocess(filename=file)
+    # filename = _preprocess(filename=file)
+    # speech_array, sample_rate = librosa.load(f"{filename}", sr=16_000)
+    print(f"\n Preprocessing COMPLETED in {round(time.time()-start_time, 2)}s \n")
+    
+    
+    start_time = time.time()
+    print("Starting Inference")
+    text = asr(speech_array)["text"]
+    # with autocast("cuda"):
+    #     text = asr(speech_array)["text"]
+    print(f"\n Inference COMPLETED in {round(time.time()-start_time, 2)}s \n")
+
+    # start_time = time.time()
+    # print("Starting to Punctuate")
+    # punctuated_text = punctuator(text)
+    # print(f"\n Punctuation COMPLETED in {round(time.time()-start_time, 2)}s \n")
+
+    return warn_output + text
+    # return warn_output + punctuated_text
+
+def _return_yt_html_embed(yt_url):
+    if "?v=" in yt_url:
+        video_id = yt_url.split("?v=")[-1].split('&')[0]
+    else:
+        video_id = yt_url.split("/")[-1].split('?feature=')[0]
+        
+    print(f"\n\nYT ID is: {video_id}\n\n")   
+    return f'<center><iframe width="500" height="320" src="https://www.youtube.com/embed/{video_id}"> </iframe> </center>'
+
+
+def yt_transcribe(yt_url):
+    start_time = time.time()
+    yt = pt.YouTube(yt_url)
+    html_embed_str = _return_yt_html_embed(yt_url)
+    stream = yt.streams.filter(only_audio=True)[0]
+    filename = "audio.mp3"
+    stream.download(filename=filename)
+    print(f"\n YT Audio Downloaded in {round(time.time()-start_time, 2)}s \n")
+    
+    
+    # for _preprocess(). No need if name of file provided in string format to asr pipeline as automatically uses ffmeg. 
+    # Only required if ndarray given by using librosa.load() to load a file
+    start_time = time.time()
+    print("Starting Preprocessing")
+    speech_array = _preprocess(filename=filename)
+    # filename = _preprocess(filename=filename)
+    # speech_array, sample_rate = librosa.load(f"{filename}", sr=16_000)
+    print(f"\n Preprocessing COMPLETED in {round(time.time()-start_time, 2)}s \n")
+    
+    start_time = time.time()
+    print("Starting Inference")
+    text = asr(speech_array)["text"]
+    # with autocast("cuda"):
+    #     text = asr(speech_array)["text"]
+    print(f"\n Inference COMPLETED in {round(time.time()-start_time, 2)}s \n")
+    
+    # start_time = time.time()
+    # print("Starting to Punctuate")
+    # punctuated_text = punctuator(text)
+    # print(f"\n Punctuation COMPLETED in {round(time.time()-start_time, 2)}s \n")
+    
+    return html_embed_str, text
+    # return html_embed_str, punctuated_text
+
+
+mf_transcribe = gr.Interface(
+    fn=transcribe,
+    inputs=[
+        gr.Audio(source="microphone", type="filepath", label="Microphone"),
+        gr.Audio(source="upload", type="filepath", label="Upload File"),
+    ],
+    outputs="text",
+    title="Whisper Bangla Demo: Transcribe Audio",
+    description=(
+        "Transcribe long-form microphone or audio inputs in BANGLA with the click of a button!"
+    ),
+    allow_flagging="never",
+)
+
+yt_transcribe = gr.Interface(
+    fn=yt_transcribe,
+    inputs=[gr.Textbox(lines=1, placeholder="Paste the URL to a Bangla language YouTube video here", label="YouTube URL")],
+    outputs=["html", "text"],
+    title="Whisper Bangla Demo: Transcribe YouTube",
+    description=(
+        "Transcribe long-form YouTube videos in BANGLA with the click of a button!"
+    ),
+    allow_flagging="never",
+)
+
+demo = gr.TabbedInterface([mf_transcribe, yt_transcribe], ["Transcribe Bangla Audio", "Transcribe Bangla YouTube Video"])
+
+
+if __name__ == "__main__":
+    
+    demo.queue()
+    demo.launch(share='True')
---
+++ wav2vec2-normal/models.py
@@ -0,0 +1,111 @@
+import os
+
+abs_path = os.path.abspath('.')
+base_dir = os.path.dirname(os.path.dirname(abs_path))
+os.environ['TRANSFORMERS_CACHE'] = os.path.join(base_dir, 'models_cache')
+
+import torch
+# Details: https://huggingface.co/docs/diffusers/optimization/fp16#enable-cudnn-autotuner
+torch.backends.cudnn.benchmark = True
+torch.backends.cuda.matmul.allow_tf32 = True
+from transformers import pipeline, AutoTokenizer, AutoFeatureExtractor, AutoConfig, AutoModelForCTC, Wav2Vec2ProcessorWithLM
+from pyctcdecode import BeamSearchDecoderCTC
+from typing import Union, BinaryIO
+from optimum.bettertransformer import BetterTransformer
+
+
+# model_name = "/home/mdrakib/asr-demo/arijit-200k-91600"
+# model_name = "Rakib/wav2vec2-xls-r-300m-bengali-commonvoice"
+model_name = "shahruk10/wav2vec2-xls-r-300m-bengali-commonvoice"
+
+
+## lets you know the device count: cuda:0 or cuda:1
+# print(torch.cuda.device_count())
+
+device = 0 if torch.cuda.is_available() else -1
+# device = -1 #Exclusively CPU
+
+print(f"Using device: {'GPU' if device==0 else 'CPU'}")
+
+if device !=0:
+    print("[Warning!] Using CPU could hamper performance")
+
+print("Loading Tokenizer for ASR Speech-to-Text Model...\n" + "*" * 100)
+# tokenizer = AutoTokenizer.from_pretrained(model_name, language=language, task=task)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+print("Loading Feature Extractor for ASR Speech-to-Text Model...\n" + "*" * 100)
+feature_extractor = AutoFeatureExtractor.from_pretrained(model_name)
+
+print("Loading Config for ASR Speech-to-Text Model...\n" + "*" * 100)
+config = AutoConfig.from_pretrained(model_name)
+
+print("Loading Decoder for ASR Speech-to-Text Model...\n" + "*" * 100)
+decoder = BeamSearchDecoderCTC.load_from_hf_hub(model_name)
+# decoder = BeamSearchDecoderCTC.load_from_dir(model_name)
+
+print("Loading Processor for ASR Speech-to-Text Model...\n" + "*" * 100)
+processor = Wav2Vec2ProcessorWithLM(
+    feature_extractor=feature_extractor,
+    tokenizer=tokenizer,
+    decoder=decoder
+)
+
+print("Loading Wav2Vec2 ASR Speech-to-Text Model...\n" + "*" * 100)
+model = AutoModelForCTC.from_pretrained(model_name)
+
+# BetterTransformer (No Need if PyTorch 2.0 works!!) 
+## (currently 2secs faster inference than PyTorch 2.0 )
+# model = AutoModelForCTC.from_pretrained(model_name)
+# model = BetterTransformer.transform(model)
+
+## bitsandbytes (only Linux & GPU) (requires conda env with conda-based pytorch!!!)
+## currently only reduces size. slower inference than native models!!!
+## from_pretrained doc: https://huggingface.co/docs/transformers/v4.25.1/en/main_classes/model#transformers.PreTrainedModel.from_pretrained
+# model = AutoModelForCTC.from_pretrained(model_name, device_map="auto", load_in_8bit=True)
+
+## For PyTorch 2.0 (Only Linux)
+# model = AutoModelForCTC.from_pretrained(model_name).to(device="cuda:0")
+##mode options are "default", "reduce-overhead" and "max-autotune". See: https://pytorch.org/get-started/pytorch-2.0/#modes
+# model = torch.compile(model, mode="default") 
+
+
+asr = pipeline(
+    task="automatic-speech-recognition",
+    model=model,
+    tokenizer=tokenizer,
+    feature_extractor=feature_extractor,
+    # processor=processor, #no effect see: https://github.com/huggingface/transformers/blob/main/src/transformers/pipelines/automatic_speech_recognition.py
+    # config=config, #no effect see: https://github.com/huggingface/transformers/blob/main/src/transformers/pipelines/automatic_speech_recognition.py
+    device=device,  # for gpu 1 for cpu -1
+    ## chunk files longer than 30s into shorted samples
+    chunk_length_s=30, 
+    ## the amount of overlap (in secs) to be discarded while stitching the inferenced chunks
+    ## stride_length_s is a tuple of the left and right stride(overlap) length.
+    ## With only 1 number, both sides get the same stride, by default
+    ## The stride_length on one side is 1/6th of the chunk_length_s if stride_length no provided
+    stride_length_s=(5, 5),
+    # stride_length_s=[6,0],
+    batch_size=16,
+    ignore_warning=True,
+    ## force whisper to generate timestamps so that the chunking and stitching can be accurate
+    # return_timestamps=True, 
+    # decoder_kwargs={"max_new_tokens": 448},  ##default is 448
+)
+
+    
+def transcribe(speech_array: Union[str, BinaryIO], language: str = "en") -> str:
+    """
+    Transcribes an audio array to text
+    Args:
+        speech_array (np.ndarray): audio in numpy array format
+        language (str): "sv" or "en"
+    Returns:
+        a string containing transcription
+    """
+        
+    result = asr(speech_array)
+
+    return str(result["text"])
+
+
